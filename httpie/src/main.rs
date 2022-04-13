e20fd6d718de9df22fcc439e0d903526d2e7bdfa
forfd8960 <forfd8960@gmail.com>
2022-04-13 22:17:17 +0800

    complete httpie

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,7 +1,9 @@
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashMap};
 use clap::{AppSettings, Parser};
 use anyhow::{Result, anyhow};
-use reqwest::Url;
+use colored::*;
+use mime::Mime;
+use reqwest::{Url, Client, Response, header};
 
 #[derive(Parser, Debug)]
 #[clap(version="1.0", author="forfd")]
@@ -37,6 +39,7 @@ struct KvPair {
     v: String,
 }
 
+// Impl fromstr trait for kvpair: can use s.parse to parse the string to kv pairs.
 impl FromStr for KvPair {
     type Err = anyhow::Error;
 
@@ -59,8 +62,67 @@ fn parse_url(s: &str) -> Result<String> {
     Ok(s.into())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()>{
     println!("httpie starts...");
     let opts: Opts = Opts::parse();
     println!("options: {:?}", opts);
+
+    let client = Client::new();
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime =  get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+fn print_status(resp: &Response) {
+    let status  = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{} {:?}", name.to_string().green(), value);
+    }
+    print!("\n");
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+
+        _ => println!("{}", body),
+    }
 }
