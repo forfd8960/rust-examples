c2389e6a82db1efe0e4e5587d610f0c388dfd0b5
forfd8960 <forfd8960@gmail.com>
2022-04-16 17:12:24 +0800

    [rust-life-cycle] add lifetime param to str_tok

--- rust-life-cycle/src/examples/str_tok.rs
+++ rust-life-cycle/src/examples/str_tok.rs
@@ -1,4 +1,4 @@
-fn strtok(s: &mut &str, delimiter: char) -> &str {
+fn strtok<'a>(s: &mut &'a str, delimiter: char) -> &'a str {
     if let Some(i) = s.find(delimiter) {
         let prefix = &s[..i];
         let suffix = &s[(i+delimiter.len_utf8())..];
@@ -14,6 +14,6 @@ fn strtok(s: &mut &str, delimiter: char) -> &str {
 fn main() {
     let s = "hello world".to_owned();
     let mut s1 = s.as_str();
-    let hello = strtok(&s1, ' ');
+    let hello = strtok(&mut s1, ' ');
     println!("hello: {}, s1: {}, s: {}", hello, s1, s);
 }
