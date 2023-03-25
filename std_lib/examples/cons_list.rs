70159779ca59b295a3762b142b1271de081d8b8d
forfd8960 <forfd8960@gmail.com>
2023-03-25 09:37:46 +0800

    Rust Box - Fix list with Box

--- std_lib/examples/cons_list.rs
+++ std_lib/examples/cons_list.rs
@@ -1,13 +1,14 @@
 use crate::List::{Cons, Nil};
 
 // https://doc.rust-lang.org/book/ch15-01-box.html
+#[derive(Debug)]
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
 // error[E0072]: recursive type `List` has infinite size
 fn main() {
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list: {:?}", list);
 }
